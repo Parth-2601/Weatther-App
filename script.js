d78d23033fd421b0e2367f02d6518139b228efa3
Parth-2601 <pmp020601@gmail.com>
2025-03-16 10:45:25 -0400

    Weather App

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiKey = "78604f113006592455d8d7f710e8d37f";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
